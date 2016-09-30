29d9bc5d80e5ed881890edf82560196330de80ad
r-vijay-r <503r.vijay@gmail.com>
2016-09-30 14:34:55 +0530

    added changes for angularfire2 test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,22 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from 'angularfire2';
+import * as firebase from 'firebase';
+import { AuthProviders,AuthMethods} from 'angularfire2';
+
+const firebaseConfig = {
+ apiKey: "AIzaSyC6IOIF7iMhwFcNUBxVwUNMgneO3nWEwnA",
+ authDomain: "angularfire2-1d8f3.firebaseapp.com",
+ databaseURL: "https://angularfire2-1d8f3.firebaseio.com/",
+ storageBucket: "gs://angularfire2-1d8f3.appspot.com"
+}
+const AuthenticationConfig = {
+   method: AuthMethods.Popup,
+   provider: AuthProviders.Google
+ }
+    AngularFireModule.initializeApp(firebaseConfig, AuthenticationConfig)
+
 @NgModule({
   declarations: [
     AppComponent
